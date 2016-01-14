176c1cd56b8aefe6347e2703b448e003530b850a
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2016-01-13 20:05:36 -0500

    alpha

--- src/GlslCanvas.js
+++ src/GlslCanvas.js
@@ -40,9 +40,6 @@ export default class GlslCanvas {
 precision mediump float;
 #endif
 
-uniform vec2 u_resolution;
-uniform float u_time;
-
 attribute vec2 a_position;
 attribute vec2 a_texcoord;
 
@@ -58,16 +55,14 @@ void main() {
 precision mediump float;
 #endif
 
-uniform vec2 u_resolution;
-uniform float u_time;
-
 varying vec2 v_texcoord;
 
 void main(){
-    vec2 st = gl_FragCoord.xy/u_resolution;
-    gl_FragColor = vec4(st.x,st.y,abs(sin(u_time)),1.0);
+    gl_FragColor = vec4(0.0);
 }
 `;
+        // Allow alpha
+        canvas.style.backgroundColor = "rgba(1,1,1,0)";
 
         // GL Context
         let gl = setupWebGL(canvas);
