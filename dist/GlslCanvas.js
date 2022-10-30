86282ead54cfe1fa3140fdc63d3e49f269b5a680
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2022-10-30 02:39:48 -0400

    0.2.4

--- dist/GlslCanvas.js
+++ dist/GlslCanvas.js
@@ -995,8 +995,6 @@ function subscribeMixin$1(target) {
 }
 
 // Texture management
-// GL texture wrapper object for keeping track of a global set of textures, keyed by a unique user-defined name
-
 var Texture = function () {
     function Texture(gl, name) {
         var options = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
@@ -1299,6 +1297,9 @@ var Texture = function () {
     return Texture;
 }();
 
+// Report max texture size for a GL context
+
+
 Texture.getMaxTextureSize = function (gl) {
     return gl.getParameter(gl.MAX_TEXTURE_SIZE);
 };
@@ -1515,7 +1516,7 @@ var GlslCanvas = function () {
                             var url = "https://lygia.xyz" + dep;
                             _this2.deps[dep] = getFile(url);
                         }
-                        _this2.fragmentString += _this2.deps[dep] + '\n#line ' + i + '\n';
+                        _this2.fragmentString += _this2.deps[dep] + '\n#line ' + (i + 1) + '\n';
                     }
                 } else _this2.fragmentString += line + '\n';
             });
